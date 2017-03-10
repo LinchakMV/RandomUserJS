0686eea4847b3c63dd37717bf5fccd022303a5f9
Michael <linchakmv@gmail.com>
2017-03-10 04:50:09 +0200

    js

--- js/text.js
+++ js/text.js
@@ -9,20 +9,3 @@ $.ajax({
     
   })},
 });
-
-// // Создаём объект запроса
-// var request = new XMLHttpRequest();
-
-//  // Указываем тип запроса (GET) и адрес, к которому будет выполнятся запрос
-// var url = "http://www.randomtext.me/api/lorem/p-1/10-50";
-// request.open("GET", url);
-
-
-// // Задаём функцию, которая будет вызываться при изменении состояния готовности запроса
-// request.onreadystatechange = function () { 
-// 	// Проверяем состояние готовности и статус запроса
-// 	if (request.readyState === 4 && request.status === 200) { 
-// 		// Десериализуем полученную JSON строку в объект JavaScript
-// 		var response = JSON.parse(request.responseText);
-// 	}
-// }
